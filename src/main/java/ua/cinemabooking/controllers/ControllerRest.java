08dc5bf0b4e544d7810cdc6f87223b839864611f
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2017-02-04 12:52:00 +0200

    Controllers view by Dima and Andy

---
+++ src/main/java/ua/cinemabooking/controllers/ControllerRest.java
@@ -0,0 +1,15 @@
+package ua.cinemabooking.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by macbookair on 04.02.17.
+ */
+
+
+@RestController("/")
+public class ControllerRest {
+
+
+
+}
