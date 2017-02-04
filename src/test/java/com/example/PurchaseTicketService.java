af062a08928b97ab94a811340d5d08cb67e5f0c6
Dmitriy Lamzin <dmitriy.lamzin@gmail.com>
2017-02-04 16:38:32 +0600

    Several Test cases

---
+++ src/test/java/com/example/PurchaseTicketService.java
@@ -0,0 +1,35 @@
+package com.example;
+
+/*
+ *
+ */
+public class PurchaseTicketService {
+
+  /**
+  * getSeatsForSession
+  * Input: session ID
+  * Output: all seats with
+  * Exception: ID not found
+ */
+
+
+  /**
+   * getFilms()
+   * @return: List of films for the day;
+   */
+
+  /**
+   * getSession(Id film);
+   *
+   * @return: List of Sessions
+   * @throws: IllegalArgumentException
+   *
+   */
+
+  /**
+   * createBillOrder(Seans Id, Place Id)
+   *
+   * returns BillOrder
+   * throws AlreadyBookedException
+   */
+}
