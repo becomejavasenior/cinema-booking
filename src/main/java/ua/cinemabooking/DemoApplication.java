e40f4810a36ab570084393cae9475f23e9d75586
GHDmitro <d.timosh1996@gmail.comd.timosh1996@gmail.comd.timosh1996@gmail.com>
2017-02-04 13:25:05 +0200

    Main class with init method

--- src/main/java/ua/cinemabooking/DemoApplication.java
+++ src/main/java/ua/cinemabooking/DemoApplication.java
@@ -2,19 +2,28 @@ package ua.cinemabooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ua.cinemabooking.service.Populator;
 
 import javax.annotation.PostConstruct;
 
+//<<<<<<< Updated upstream
+//=======
+
+//>>>>>>> Stashed changes
 @SpringBootApplication
 public class DemoApplication {
+
+
+	@PostConstruct
+	public  void  init() {
+		System.out.println("ok");
+		new Populator().init();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@PostConstruct
-	public  void  init() {
-		System.out.println("ok");
-//		new Populator().populate();
-	}
+
 }
