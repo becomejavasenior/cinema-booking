1638cbafb507ce4c35a352e5acc4c86b273875b4
r.a.t.i.b.o.r@mail.ru <ratibor92>
2017-02-04 13:31:39 +0200

    getAllSeats impl

--- src/main/java/ua/cinemabooking/service/TiketsServiceImpl.java
+++ src/main/java/ua/cinemabooking/service/TiketsServiceImpl.java
@@ -5,10 +5,12 @@ import ua.cinemabooking.model.BillOrder;
 import ua.cinemabooking.model.Movie;
 import ua.cinemabooking.model.Place;
 import ua.cinemabooking.model.Seans;
+import ua.cinemabooking.repository.BillOrderRepository;
 import ua.cinemabooking.repository.SeansRepository;
 import ua.cinemabooking.serviceModel.Seats;
 
 import javax.persistence.criteria.Order;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,11 +19,30 @@ import java.util.List;
 public class TiketsServiceImpl implements  TiketsService {
     @Autowired
     SeansRepository seansRepository;
+    @Autowired
+    BillOrderRepository billOrderRepository;
     @Override
-    public Seats getAllSeats(Movie movie, Seans seans) {
-        List<Seans> seansList = seansRepository.findByMovie(movie).;
+    public Seats getAllSeats(Seans seans) {
+        List<BillOrder> orderList = billOrderRepository.findBySeans(seans);
+        List<Boolean> seats = new ArrayList<>();
+        for (int x = 0; x <10; x++) {
+            for (int y = 0; y <10 ; y++) {
+                Boolean resalt = true;
+                for (BillOrder order: orderList
+                     ) {
+                    if (order.getPlace().getX()==x && order.getPlace().getY()==y && order.isPayed())
+                        resalt = false;
+                }
+                seats.add(resalt);
 
-        return null;
+            }
+        }
+        Seats seats1= new Seats();
+        seats1.setMap(seats);
+        seats1.setPrice(seans.getMovie().getPrice());
+        seats1.setFilmDate(seans.getStart());
+        seats1.setFilmName(seans.getMovie().getName());
+        return seats1;
     }
 
     @Override
