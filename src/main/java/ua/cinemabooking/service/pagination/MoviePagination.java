801daa666bd1614a0522f385af3323aba4c94ba1
Popov85 <andrey.popov85@gmail.com>
2017-02-18 12:37:33 +0200

    Server-side pagination

--- src/main/java/ua/cinemabooking/service/pagination/MoviePagination.java
+++ src/main/java/ua/cinemabooking/service/pagination/MoviePagination.java
@@ -1,7 +1,10 @@
 package ua.cinemabooking.service.pagination;
 
+
+import ua.cinemabooking.model.Movie;
+
 /**
  * Created by Andrey on 2/18/2017.
  */
-public class MenuPagination {
+public class MoviePagination extends Pagination<Movie> {
 }
--- src/main/java/ua/cinemabooking/service/pagination/Pageable.java
+++ src/main/java/ua/cinemabooking/service/pagination/Pageable.java
@@ -1,8 +1,11 @@
 package ua.cinemabooking.service.pagination;
 
+import java.util.List;
+
 /**
  * Created by Andrey on 2/18/2017.
  */
-public interface getAll {
-        
+public interface Pageable<T> {
+        List<T> getAll(int start, int length);
+
 }
