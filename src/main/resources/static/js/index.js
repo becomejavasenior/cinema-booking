5a6fd276ab8a3675d2426cf57642ab053e50baf5
Popov85 <andrey.popov85@gmail.com>
2017-02-20 20:36:01 +0200

    Front finished

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,9 @@
 /**
  * Created by Andrey on 2/19/2017.
  */
+
 $(document).ready(function () {
-    var count= -1;
+    window.count= -1;
     var table = $('#movies').DataTable({
         "bLengthChange": false,
         "bAutoWidth": false,
@@ -14,31 +15,37 @@ $(document).ready(function () {
         serverSide:true,
         columns: [
             { "data": null, "title": "", "visible": false, "render": function(data){
-                count++;
+                window.count++;
+                console.log("count="+count);
                 return drawSection(data, count);
             }
             }
         ]
     });
-    $('#movies').on('page.dt', function() {
-        // TODO erase all current clones on the page
-        var info = table.page.info();
-        var page = info.page+1;
-        console.log('changed - page '+page+' out of '+info.pages+' is clicked');
-    });
+
 });
+
+$(document).ready( function () {
+    $('#movies').on('page.dt', function() {
+        $("div.row.my-content-row").remove();
+        window.count=-1;
+    });
+} );
+
 
 function drawSection(response, counter){
 
-    let rowdiv = $('<div class="row" id="myrow"></div>');
+    let rowdiv = $('<div class="row my-content-row"></div>');
 
-        let $lastRow = getLastRow();
+    $('.container').find(".my-header").after(rowdiv);
+
+        let $lastRow = getLastRow();  //поиск последней строки
 
         let newRow;
 
-        if ((counter % 3) === 0){
+        if ((counter % 3) === 0) {
 
-            $('.container').find('#upperrow').last().after(rowdiv);
+            $('.container').find('.my-content-row').last().after(rowdiv);
 
             $lastRow = rowdiv;
 
@@ -48,7 +55,7 @@ function drawSection(response, counter){
 
         let $curItem = getCloneItem();
 
-        $curItem.find('h3').find('a').attr('href', '/seans/'+$movieItem.id).empty().append('<h4>'+$movieItem.name+'</h4>');
+        $curItem.find('h3').find('a').attr('href', '/seans/' + $movieItem.id).empty().append('<h4>' + $movieItem.name + '</h4>');
 
         $curItem.show();
 
@@ -56,12 +63,14 @@ function drawSection(response, counter){
 
         getLastRow().val($lastRow);
 
+    console.log("New line has been drawn!");
+
     return "ok";
 
 }
 
 function getLastRow() {
-    return $('.container').find('#myrow').last();
+    return $('.container').find('.my-content-row').last();
 }
 
 function getCloneItem() {
